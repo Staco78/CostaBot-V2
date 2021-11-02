2c84429b74ff58382399c4fb0bb1b8e02f6a18ce
Staco78 <contact@staco-dev.fr>
2021-11-02 22:23:00 +0100

    removed crash report

--- src/index.ts
+++ src/index.ts
@@ -4,34 +4,13 @@ import { Database } from "./data/database";
 import { execSync } from "child_process";
 import Server from "./server";
 import Constants from "./common/constants";
-import { writeFile } from "fs/promises";
 
 process.title = "CostaBot";
-
-process.on("uncaughtException", async e => {
-    await reportE(e);
-});
-
-process.on("exit", async () => {
-    await reportE(null);
-});
-
-process.on("unhandledRejection", async e => {
-    await reportE(e as any);
-});
-
-async function reportE(e: Error | null) {
-    const date = new Date();
-    await writeFile(
-        `crash report ${date.toDateString()} ${date.getHours()}h${date.getMinutes()} ${date.getSeconds()}s`,
-        e?.name + "\n" + e?.message + "\n\n\n" + e?.stack
-    );
-}
 
 try {
     execSync("ffmpeg -version");
 } catch (e) {
-    console.log("\x1b[31m%s\x1b[0m", "\n\nFfmpeg not found\nPlease reinstall it or add it to the path (windows)\n\n");
+    console.log("\x1b[31m%s\x1b[0m", "Ffmpeg not found\nPlease reinstall it or add it to the path");
     process.exit(-1);
 }
 
